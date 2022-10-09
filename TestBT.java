222bd153b38fa991188d650b2d3ccb2f1b9e7c16
Alioucbd <40865590+Alioucbd@users.noreply.github.com>
2022-10-09 18:15:09 +0200

    Update TestBT.java

--- TestBT.java
+++ TestBT.java
@@ -2,12 +2,12 @@ public class Test{
 	public static void main(String args[]) {
 		SudokuBT test = new SudokuBT(2);
 		int grille[][] =
-    {
-        {1, 0, 3, 4},
-		{3, 4, 0, 2},
-		{2, 1, 4, 0},
-		{0, 3, 2, 1}
-    };
+		    {
+			{1, 0, 3, 4},
+			{3, 4, 0, 2},
+			{2, 1, 4, 0},
+			{0, 3, 2, 1}
+		    };
 		test.estValide(grille,0);
 		//test.findSolution(0,0);
 	}
